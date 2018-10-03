f87f64c989f8b7db63b3366a6d1a021fa57687b3
elmarolafss <elmarol250@nemi.tskoli.is>
2018-10-03 08:55:06 +0000

    almost done

--- Verkefni-3/verkefni3.js
+++ Verkefni-3/verkefni3.js
@@ -1,16 +1,23 @@
-// We need 3 things everytime we use Three.js
- // Scene + Camera + Renderer
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 const renderer = new THREE.WebGLRenderer({ antialias: true})
+var controls;
 
 renderer.setSize( window.innerWidth, window.innerHeight )
-// sets renderer background color
+
 renderer.setClearColor("#00b894")
 document.body.appendChild( renderer.domElement )
 camera.position.z = 6
+// drag controls
+controls = new THREE.TrackballControls( camera );
+controls.rotateSpeed = 5;
+controls.zoomSpeed = 10;
+controls.panSpeed = 10;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 1;
 
-// resize canvas on resize window
 window.addEventListener( 'resize', () => {
 	let width = window.innerWidth
 	let height = window.innerHeight
@@ -20,25 +27,26 @@ window.addEventListener( 'resize', () => {
 })
 
 // basic cube
-var geometry = new THREE.BoxGeometry( 1, 2, 2)
+var geometry = new THREE.BoxGeometry( 1, 3, 1)
 var material = new THREE.MeshStandardMaterial( { color: 0x636e72, flatShading: true, metalness: 0, roughness: 1 })
 var cube = new THREE.Mesh ( geometry, material )
 scene.add( cube )
 
 // ambient light
-var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.5)
+var ambientLight = new THREE.AmbientLight ( 0xf12300ff, 2)
 scene.add( ambientLight )
 
 // point light
-var pointLight = new THREE.PointLight( 0xffffff, 0.7 );
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
 pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
 
+
+
 function animate() {
 	requestAnimationFrame( animate )
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.04;
 	renderer.render( scene, camera )
+	controls.update();
 }
 animate()
