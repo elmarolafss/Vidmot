7d8174bfa061c31b624693f36dc3030eaa9e197b
elmarolafss <elmarol250@nemi.tskoli.is>
2018-10-05 14:19:53 +0000

    done

--- Verkefni-3/verkefni3.js
+++ Verkefni-3/verkefni3.js
@@ -1,11 +1,15 @@
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const camera = new THREE.PerspectiveCamera( 65, window.innerWidth / window.innerHeight, 0.1, 1000 )
 const renderer = new THREE.WebGLRenderer({ antialias: true})
+var geometry = new THREE.BoxGeometry( 2, 2, 2)
+var material = new THREE.MeshStandardMaterial( { color: 0x00cec9, flatShading: false, metalness: 0, roughness: 5 })
+var cube = new THREE.Mesh ( geometry, material )
 var controls;
 
 renderer.setSize( window.innerWidth, window.innerHeight )
 
 renderer.setClearColor("#00b894")
+// renderer.setClearColor("#000000")
 document.body.appendChild( renderer.domElement )
 camera.position.z = 6
 // drag controls
@@ -15,7 +19,7 @@ controls.zoomSpeed = 10;
 controls.panSpeed = 10;
 controls.noZoom = false;
 controls.noPan = false;
-controls.staticMoving = true;
+controls.staticMoving = false;
 controls.dynamicDampingFactor = 1;
 
 window.addEventListener( 'resize', () => {
@@ -27,26 +31,28 @@ window.addEventListener( 'resize', () => {
 })
 
 // basic cube
-var geometry = new THREE.BoxGeometry( 1, 3, 1)
-var material = new THREE.MeshStandardMaterial( { color: 0x636e72, flatShading: true, metalness: 0, roughness: 1 })
-var cube = new THREE.Mesh ( geometry, material )
+
 scene.add( cube )
 
 // ambient light
-var ambientLight = new THREE.AmbientLight ( 0xf12300ff, 2)
-scene.add( ambientLight )
+ var ambientLight = new THREE.AmbientLight ( 0xffffff, 1)
+ ambientLight.position.set(20,20,20)
+ scene.add( ambientLight )
 
 // point light
-var pointLight = new THREE.PointLight( 0xffffff, 1 );
-pointLight.position.set( 25, 50, 25 );
+var pointLight = new THREE.PointLight( 0xffffff, 2 );
+pointLight.position.set( 50, 50, 50 );
 scene.add( pointLight );
 
 
-
+let peepee = 0;
 
 function animate() {
-	requestAnimationFrame( animate )
+	pointLight.position.z = peepee;
+	if(peepee >= 500) { peepee = 0 }
+	else { peepee += 1 }
 	renderer.render( scene, camera )
 	controls.update();
+	requestAnimationFrame( animate )
 }
 animate()
